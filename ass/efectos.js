b356dd6396adef5ea99d1dd45168d6c4b55dadd4
Cesar Gomez <cesargomez060690@gmail.com>
2020-12-10 10:27:10 -0300

    Sumamos la seccion servicios

---
+++ ass/efectos.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    setTimeout(function (){
+        document.getElementById('hero-background').style.width = '100%';
+    }, 1500);
+    setTimeout(function (){
+        document.getElementById('hero-text').style.opacity = '1';
+    }, 2500);
+    setTimeout(function (){
+        document.getElementById('hero-img').style.opacity = '1';
+    }, 3500);
+    setTimeout(function (){
+        document.getElementById('lines').style.opacity = '1';
+    }, 4500);
+}
